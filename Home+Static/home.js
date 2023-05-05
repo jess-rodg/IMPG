7b433ca46b9984644d4fb803ba0b3148b0834179
qmc-aubr <114527842+qmc-aubr@users.noreply.github.com>
2023-05-05 22:22:09 +0100

    Add files via upload

---
+++ Home+Static/home.js
@@ -0,0 +1,12 @@
+document.getElementById("one").addEventListener("click", () => {
+   document.getElementById("one").src = 'clothes.png';
+})
+document.getElementById("two").addEventListener("click", () => {
+   document.getElementById("two").src = 'rice.jpeg';
+})
+document.getElementById("three").addEventListener("click", () => {
+   document.getElementById("three").src = 'facial.jpeg';
+})
+document.getElementById("four").addEventListener("click", () => {
+   document.getElementById("four").src = 'maho.jpeg';
+})
