824df747bd596b1f913a4e3369f9a0f14db03b71
Jess Rodgers <jess9rodgers@gmail.com>
2023-05-21 22:18:44 +0100

    got rid of quiz quotation marks

--- Atayal website/js/quiz.js
+++ Atayal website/js/quiz.js
@@ -50,7 +50,7 @@ $(document).ready(function () {
         } else {
             $("#resultPlaceholder").html("<h2>Incorrect!</h2>");
             $("#resultPlaceholder").css("color", "red")
-            $("#incorrectSentencePlaceholder").html("<p>" + JSON.stringify(question.incorrectSentence) + "</p>");
+            $("#incorrectSentencePlaceholder").html("<p>" + (question.incorrectSentence) + "</p>");
         }
         $(question.answers[option].correct).css("background-color", "green");
         $(question.answers[option].incorrect).css("background-color", "red");
@@ -64,10 +64,10 @@ $(document).ready(function () {
             let x = Math.floor(Math.random() * questions.questions.length);
             question = questions.questions[x];
             questions.questions.splice(x, 1);
-            $("#questionPlaceholder").html("<p>" + JSON.stringify(question.text) + "</p>");
-            $("#answerButton1").html(JSON.stringify(question.answers[0].text));
-            $("#answerButton2").html(JSON.stringify(question.answers[1].text));
-            $("#answerButton3").html(JSON.stringify(question.answers[2].text));
+            $("#questionPlaceholder").html("<p>" + (question.text) + "</p>");
+            $("#answerButton1").html((question.answers[0].text));
+            $("#answerButton2").html((question.answers[1].text));
+            $("#answerButton3").html((question.answers[2].text));
             $("#answerButton1").prop("disabled", false);
             $("#answerButton2").prop("disabled", false);
             $("#answerButton3").prop("disabled", false);
