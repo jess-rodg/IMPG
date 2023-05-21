e23aba8cf1af8aea34b2d4f09a2ebeb058b0f8b7
qmc-aubr <mq688@york.ac.uk>
2023-05-22 00:03:46 +0100

    borders adjusted

--- Atayal website/js/intro.js
+++ Atayal website/js/intro.js
@@ -23,11 +23,11 @@ function collectCharChoice(event) {
 let playerName = document.getElementById('player-name');
 playerName.addEventListener('submit', playerNameSubmit);
 function playerNameSubmit(event) {
-
     // Cannot advance if a charater isn't selected 
     if(userData[0] === 'image location'){
         event.preventDefault();
         alert('Please click on a character.');
+        
     }
     // Move on if character selected
     else {
@@ -37,6 +37,13 @@ function playerNameSubmit(event) {
         window.location.replace('gameapp.html');
     }
 }
-
+document.getElementById("female").addEventListener("click", () => {
+    document.getElementById("male").style.scale=0.8;
+    document.getElementById("female").style.scale=1.1;
+})
+document.getElementById("male").addEventListener("click", () => {
+    document.getElementById("female").style.scale=0.8;
+    document.getElementById("male").style.scale=1.1;
+})
 
 
